bbefb7990b521c74e268c40b80e3ac7823bc0fbc
Sergey Akatov <2287485@gmail.com>
2024-05-02 12:55:33 +0300

    Finished .py file

--- processing_applications.py
+++ processing_applications.py
@@ -16,6 +16,7 @@ appl = input('Введите заявку')
 # extracting dates from application
 date_pattern = r'\b\d{2}\.(?:0[1-9]|1[0-2])(?:\.\d{2}(?:\d{2})?)?\b'
 dates = re.findall(date_pattern, appl)
+dates = dates[0]
 
 
 def convert_to_full_year(date_str):
@@ -41,7 +42,7 @@ def convert_to_full_year(date_str):
 
 
 # assigning a variable with a date
-convert_date = convert_to_full_year(dates[0])
+convert_date = convert_to_full_year(dates)
 
 
 def find_manager(text, managers_df):
@@ -180,4 +181,141 @@ def find_transshipment(text):
 transshipment_found = find_transshipment(appl)
 
 
+def find_purchaser(text):
+    """
+    Function for determining the purchaser's name
+    """
+    purchaser_match = \
+        re.search(r'(?i)\d+\.\s*(Покупатель\s*(груза)?\s*(:)?)\s*(.+?)\n',
+                  text)
+    purchaser = purchaser_match.group(4) if purchaser_match else np.NAN
+
+    return purchaser
+
+
+# assigning a variable with the purchaser's name
+purchaser_found = find_purchaser(appl)
+
+
+def find_consignee(text):
+    """
+    Function for determining the consignee's name
+    """
+    consignee_match = \
+        consignee_match = re.search(
+        r'(?i)\d+\.\s*(?:Грузополучатель\s*(?::)?|Грузополучатель\s*\(при\s*оформлении\s*ттн\)\s*(?::)?)\s*(.+?)\n',
+        text)
+    consignee = consignee_match.group(1).split(':')[-1].strip() if consignee_match else np.NAN
+
+    return consignee
+
+
+# assigning a variable with the consignee's name
+consignee_found = find_consignee(appl)
+
+
+def find_consignee_leg_addr(text):
+    """
+    Function for determining the legal consignee's address
+    """
+    find_consignee_leg_addr_match = \
+        re.search(r'(?i)\d+\.\s*(Юр\s*\.)?\s*(Адрес\s*грузополучателя)\s*(\(юрид\))?\s*(:)?\s*(.+?)\n',
+                  text)
+    find_consignee_leg_addr = find_consignee_leg_addr_match.group(5) if find_consignee_leg_addr_match else np.NAN
+
+    return find_consignee_leg_addr
+
+
+# assigning a variable with legal consignee's address
+consignee_leg_addr_found = find_consignee_leg_addr(appl)
+
+
+def find_phones(text):
+    """
+    Function for determining the phone numbers
+    """
+    phone_numbers = re.findall(r'\+?\d{1,3}[\s-]?\(?\d{3}\)?[\s-]?\d{2,3}[\s-]?\d{2}[\s-]?\d{2}(?=\s|$)', text)
+    phone_numbers = phone_numbers if phone_numbers else np.NAN
+
+    return phone_numbers
+
+
+# assigning a variable with phone numbers and writing them without list brackets
+phones_found = ', '.join(find_phones(appl)) if find_phones(appl) is not np.NAN else np.NAN
+
+
+def find_unl_addr(text, unload_addresses_df):
+    """
+    Function for determining unload addresses
+    """
+    for index, row in unload_addresses_df.iterrows():
+        pattern1 = row['pattern1']
+        pattern2 = row['pattern2']
+        address = row['address']
+
+        if pattern1.lower().replace(' ', '') and pattern2.lower().replace(' ', '') \
+                in text.lower().replace(' ', ''):
+            return address
+    return np.NAN
+
+
+# assigning a variable with unload addresses
+unl_addr_found = find_unl_addr(appl, unload_addresses)
+
+
+def find_time(text):
+    """
+    Function for determining the unloading time
+    """
+    time_match = re.search(r'(?i)(время)?\s*при(ё|е)мк(и|а)(?::)?\s*(.*)', text)
+    time = time_match.group(4) if time_match else np.NAN
+
+    return time
+
+
+# assigning a variable with unloading time
+accept_time_found = find_time(appl)
+
+
+def find_note(text):
+    """
+    Function for determining the note
+    """
+    note_match = re.search(r'(?i)(.*)\s*(?::)?\s*(оплата)\s*(?::)?\s*(.*)', text)
+    note = '{} {} {}'.format(note_match.group(1).strip(), note_match.group(2).strip(),
+                             note_match.group(3).strip()) if note_match else np.NAN
+
+    return note
+
+
+# assigning a variable with the note
+note_found = find_note(appl)
+
+
+# creating new df with a string
+new_row_df = pd.DataFrame({'Менеджер': [manager_found],
+                           'Вид доставки': [delivery_found],
+                           'Дата': [convert_date],
+                           'Товар': [product_found],
+                           'Примечание к Товару': [product_notice_found],
+                           'Кол-во': [quantity_found],
+                           'Ед.изм.': [unit_found],
+                           'Машина/Водитель': [cars_found],
+                           'Продавец': [organization_found],
+                           'Откуда': [transshipment_found],
+                           'Покупатель': [purchaser_found],
+                           'Грузополучатель': [consignee_found],
+                           'Адрес грузополучателя (юрид)': [consignee_leg_addr_found],
+                           'Адрес пункта разгрузки': [unl_addr_found],
+                           'Контакт гп': [phones_found],
+                           'Время приемки': [accept_time_found],
+                           'Примечание Иное': [note_found],
+                          })
+
+# concatenating dfs
+df = pd.concat([df, new_row_df], ignore_index=True)
+
+# record updated df
+with pd.ExcelWriter("appl_register.xlsx") as writer:
+    df.to_excel(writer, sheet_name="data", index=False)
 
